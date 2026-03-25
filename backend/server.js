bc2583d0c86fd522ba92c202c2ab285e19cf024f
Ashwani Raghav <ashwani.raghav_cs23@gla.ac.in>
2026-03-25 10:12:57 +0530

    userLogout

--- backend/server.js
+++ backend/server.js
@@ -4,5 +4,5 @@ const app = require('./src/app');
 const connectDB = require('./src/db/db');
 connectDB();
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+  console.log('Server is starting on port 3000');
 });
