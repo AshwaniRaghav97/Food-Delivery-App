95a49d50367483af39faab8660acd4743ea22893
Ashwani Raghav <ashwani.raghav_cs23@gla.ac.in>
2026-04-14 21:06:49 +0530

    changes on port

---
+++ backend/src/controllers/food.controller.js
@@ -0,0 +1,7 @@
+const foodModel = require('../models/fooditem.model');
+
+async function createFood(req,res){}
+
+module.exports = {
+    createFood
+};
---
+++ backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,25 @@
+const foodPartnerModel = require("../models/foodPartner.model");
+const jwt = require('jsonwebtoken');
+
+async function authenticateFoodPartner(req,res,next){
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({ message: 'please login first' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const foodPartner = await foodPartnerModel.findById(decoded.id);
+    if (!foodPartner) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+    req.foodPartner = foodPartner;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+}
+
+module.exports = {
+  authenticateFoodPartner
+}
